07cd891e747d5e687db2638b96bba943aa34dbce
uzaxirr-fam <129287295+uzaxirr-fam@users.noreply.github.com>
2023-08-10 16:11:32 +0530

    changes

--- src/programs/wba_prereq.rs
+++ src/programs/wba_prereq.rs
@@ -1,63 +1,88 @@
 use solana_idlgen::idlgen;
 
-idlgen!(
-    "version": "0.1.0",
-    "name": "wba_prereq",
-    "metadata": {
-        "address": "HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1"
+idlgen!({
+  "version": "0.1.0",
+  "name": "wba_prereq",
+  "instructions": [
+    {
+      "name": "complete",
+      "accounts": [
+        {
+          "name": "signer",
+          "isMut": true,
+          "isSigner": true
         },
-    "instructions": [
         {
-            "name": "complete",
-            "accounts": [
-                {
-                    "name": "signer",
-                    "isMut": true,
-                    "isSigner": true
-                },
-                {
-                    "name": "prereq",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "systemProgram",
-                    "isMut": false,
-                    "isSigner": false
-                }
-            ],
-            "args": [
-                {
-                    "name": "github",
-                    "type": "bytes"
-                }
-            ]
-        }
-        // Add other instructions here if needed
-    ],
-    "accounts": [
+          "name": "prereq",
+          "isMut": true,
+          "isSigner": false
+        },
         {
-            "name": "PrereqAccount",
-            "type": {
-                "kind": "struct",
-                "fields": [
-                    {
-                        "name": "github",
-                        "type": "bytes"
-                    },
-                    {
-                        "name": "key",
-                        "type": "publicKey"
-                    }
-                ]
-            }
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
         }
-    ],
-    "errors": [
+      ],
+      "args": [
         {
-            "code": 6000,
-            "name": "InvalidGithubAccount",
-            "msg": "Invalid Github account"
+          "name": "github",
+          "type": "bytes"
         }
-    ]
-);
+      ]
+    },
+    {
+      "name": "update",
+      "accounts": [
+        {
+          "name": "signer",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "prereq",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "github",
+          "type": "bytes"
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "PrereqAccount",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "github",
+            "type": "bytes"
+          },
+          {
+            "name": "key",
+            "type": "publicKey"
+          }
+        ]
+      }
+    }
+  ],
+  "metadata": {
+    "address": "HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1"
+  },
+  "errors": [
+    {
+      "code": 6000,
+      "name": "InvalidGithubAccount",
+      "msg": "Invalid Github account"
+    }
+  ]
+});
